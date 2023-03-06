f43f479155b8125c6824bffc5965cda3078eac1f
OmSingh-492 <singh.om.mail01@gmail.com>
2023-03-07 01:57:10 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GSoC 2023 Qualification Task`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
